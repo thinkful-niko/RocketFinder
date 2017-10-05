01623b952250a1773c65abf1e9005529dda939e9
Nicolas <nkao552@gmail.com>
2017-10-05 14:46:05 -0700

    API Hack

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function getData(num){
   
   $.ajax({
     type:"GET",
-    url:`https://launchlibrary.net/1.2/launch/next/${num}`, // `${num} gets the num parameter which is the value from the slider input, (e) on line 28`
+    url:`https://launchlibrary.net/1.2/launch/next/${num}`, // `${num} gets the num parameter which is the value from the slider input, (e) on line 27`
     success: function(data) {
       //$('.text').text(JSON.stringify(data));
       console.log(data);
@@ -26,14 +26,14 @@ $(document).ready(function(){
 //Get value for # of launches
 function showValue(newValue) { document.getElementById("range").innerHTML=newValue; }
 
-$("input").on('change', (e) => { //once the slider (input) changes, it fetches e value on line 36
+$("input").on('change', (e) => { //once the slider (input) changes, it fetches e value on line 37
 	
 	for (var i = 0; i < markers.length; i++) { //loops through array of markers and removes them from earth element;
         	
         	markers[i].removeFrom(earth);
         }
     markers = [];
-	//deleteMarkers();
+
  	getData(e.target.value); //gets value from (e) and send it to getData as the num parameter
  });
 
@@ -43,14 +43,7 @@ var earth = new WE.map('earth_div', options);
 var markers = [];
 
 function initialize2() { // webGL initialize, will eventually get removed with the exeption of tilelayer and other lines.
-		//var marker = WE.marker([51.5, -0.09]).addTo(earth);
-        //marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
-
-        //var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
-        //marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
-
-        //var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
-
+		
         WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
            minZoom: 0,
            maxZoom: 5,
@@ -68,7 +61,7 @@ function displayMarkers(data){ //gets data (JSON) from getData function
 
 			var marker = WE.marker([latitude, longitude]).addTo(earth); // creates marker variable for earthGL
 			markers.push(marker); //pushes marker inside an array of markers[] so it can be removed with the change of teh slider;
-        	marker.bindPopup(item.name, {maxWidth: 150, closeButton: true}).openPopup();// Popup description inside the marker, at the moment set to name only.
+        	marker.bindPopup("<p><b>Rocket name: </b><br>" + item.name+ "<br><hr><b>Launch date: <br></b>" + item.net , {maxWidth: 150, closeButton: true}).openPopup();// Popup description inside the marker, at the moment set to name only.
         	console.log(markers);
         });
         window.markers = markers;
