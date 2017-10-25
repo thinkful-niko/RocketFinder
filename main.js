c59a8e23dc6943e0e5a8b6a874a5bfd80d2d850d
Nicolas <nkao552@gmail.com>
2017-10-24 17:21:48 -0700

    Cleaning up code

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
+// Global variables
+var options = {
+    atmosphere: true,
+    center: [0, 0],
+    zoom: 0
+};
+var earth = new WE.map('earth_div', options);
+var markers = [];
+let stop = false;
 let allData;
 
+// Media Query
 const mq = window.matchMedia( "(max-width: 720px)" );
 
+// Document Preparation
 $("#note").html("");
 
+$('input#navigation+label').css("visibility", "hidden");
+
+function showValue(newValue) {
+    document.getElementById("range").innerHTML = newValue;
+}
+
+//Get Request for LaunchLibrary JSON of launches
 function getData(num) {
     $('.text').text('loading . . .');
 
     $.ajax({
         type: "GET",
-        url: `https://launchlibrary.net/1.2/launch/next/${num}`, // `${num} gets the num parameter which is the value from the slider input, (e) on line 27`
+        url: `https://launchlibrary.net/1.2/launch/next/${num}`, 
         success: function(data) {
-            //$('.text').text(JSON.stringify(data));
             console.log(data);
-            displayMarkers(data, true); //run marker display function, which fetches latitude and longitude from JSON and creates a marker from that; The "true" parameter allows line 86 to run.
+            displayMarkers(data, true);
             allData = data;
             return data;
         },
@@ -28,71 +45,116 @@ $(document).ready(function() {
     initialize2();
 });
 
-function displayMarkers(data, first) { //gets data (JSON) from getData function and creates map markers & left sidebar list items. "first" parameter is a boolean that will either allow or stop #launchlist event from appending more li to html
+//Create Markers, Left List Items, "first" parameter blocks or enables .append method with a boolean
+function displayMarkers(data, first) { 
+    data.launches.forEach((item, i) => { 
 
-
-    //console.log(data);
-    
-    data.launches.forEach((item, i) => { // loops through JSON, items are the elements in the array, i is the # of the item inside the array
-
-
-        let longitude = item.location.pads[0].longitude; //fetches longitutde and latitude from JSON, pads are always [0];
+        //Finds location and marks it on Earth
+        let longitude = item.location.pads[0].longitude; 
         let latitude = item.location.pads[0].latitude;
 
-        var marker = WE.marker([latitude, longitude]).addTo(earth); // creates marker variable for earthGL
-        markers.push(marker); //pushes marker inside an array of markers[] so it can be removed with a change of the slider;
+        var marker = WE.marker([latitude, longitude]).addTo(earth); 
+        markers.push(marker); 
         marker.bindPopup("<div class='blob'><p><b>Rocket name | Payload: </b><br>" + item.name + "</p></div>", {
             maxWidth: 150,
             closeButton: true
-        }); // Popup description inside the marker, at the moment set to name only.
+        }); 
     
         if (data.launches.length == 1){
           marker.openPopup();
         }
 
-
-        if (first) { // if statement stops this part of function from running on the #launchlist li click function bellow
+        //Populates Left List With Lauch Info
+        if (first) { 
             $("#launchList").append(
-                "<li class='jsLaunch" + " " + "sideBarItem'" + "><a href='#'><b>" // I'm adding two classes in case I want to do live update of the list, "sideBarItem" is reserved for styling.
+                "<li class='jsLaunch" + " " + "sideBarItem'" + "><a href='#'><b>"
                 +
                 "<p>" + item.rocket.name + "</b>" + "</p>" +
                 "<p>" +item.net + "</p>" +
                 "<p>" +item.location.name + "</p>" +
-                // + "<a href='" + item.rocket.wikiURL + "' target='_blank'>" + "About Rocket" + "</a>" +"<br>" 
-                // + "<a href='" + item.location.pads[0].mapURL + "' target='_blank'>" + "Map" + "</a>" + "<br>" 
-                "</a></li>" //End
-
-
-
-            ); //This will eventually be the sidebar populated w/ more info
+                "</a></li>"
+            );
         };
     });
 
-    window.markers = markers; //probably unecessary at this point, but I'll leave it here.
+
 }
 
-//Show value for # of launches
-function showValue(newValue) {
-    document.getElementById("range").innerHTML = newValue;
+// Handles Globe Animation
+function animation(vel){
+    var before = null;
+    requestAnimationFrame(function animate(now) {
+                var c = earth.getPosition();
+                var elapsed = before ? now - before : 0;
+                before = now;
+                earth.setCenter([c[0], c[1] + vel * (elapsed / 30)]);
+                if (stop) {
+                    return
+                };
+                requestAnimationFrame(animate);
+    });
 }
 
-$('input#navigation+label').css("visibility", "hidden"); //Hide Slide Menu Button
+//Initializes WebGL Earth with NASA Sattelite Tile + Starts Globe Animation On Startup
+function initialize2() {
+    WE.tileLayer('https://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', { 
+        minZoom: 0,
+        maxZoom: 5,
+        attribution: 'NASA'
+    }).addTo(earth);
+
+    animation(0.4);
+}
 
 
-// Slider Functionality and Value
-$("input.sliderControl").on("change", (e) => { //once the slider (input) changes, it fetches "e" value and sends it to getData
+//EVENT ACTIVATED FUNCTIONS-------------------------------------------------------------------------------------------------------------------------
 
-    for (var i = 0; i < markers.length; i++) { //loops through array of markers and removes them from earth element;
+
+
+//-------------Stops Animation once user interacts with Earth and waits before restarting
+$("#earth_div").click(function() {
+
+     stop = true;
+     setTimeout(function(){
+            stop = false; //Allow animation after closing "focus"/infoWindow
+                    
+            animation(0.2);
+        
+        }, 3500);
+})
+
+
+
+//-------------Clears or Adds "active" class on li element.
+$("#launchList").on("click", "li", function(e){
+  if($("#launchList>li").hasClass("active") && $("#launchList>li>a>p").hasClass("activeCol")){
+    $("#launchList>li").removeClass("active");
+    $("#launchList>li>a>p").removeClass("activeCol");
+    $(this).addClass("active");
+    $(this).children("a").children("p").addClass("activeCol");
+  }else{
+    $(this).addClass("active");
+    $(this).children("a").children("p").addClass("activeCol");
+  }
+});
+
+
+
+//-------------Range Input Functionality
+$("input.sliderControl").on("change", (e) => { 
+    for (var i = 0; i < markers.length; i++) {
 
         markers[i].removeFrom(earth);
-        $(".jsLaunch").remove(); //Currently this removes all list items and resets the list to fit the "e" value. Should I implement live update through a loop?
+        $(".jsLaunch").remove();
     }
-  
-    $('input#navigation+label').css("visibility", "initial");// Show Button after moving slider
-    $("#startScreen").addClass("fadeOut"); //Fades start screen
+    
+    //Animates Opening and Left List
+    $('input#navigation+label').css("visibility", "initial");
+    $("#startScreen").addClass("fadeOut");
     $(".controller p").addClass("fontSize");
 
-    if (mq.matches) { // media query
+    //MEDIA QUERY
+    if (mq.matches) {
         setTimeout(function(){
             $(".controller h2").addClass("displayNone");
             $("footer").addClass("moveDown");
@@ -100,64 +162,39 @@ $("input.sliderControl").on("change", (e) => { //once the slider (input) changes
     }
 
 
-
+    // Clears Invisible startScreen
     setTimeout(function(){
       $("#startScreen").addClass("displayNone");
-    }, 2200); //Clears invisible startscreen
+    }, 2200);
 
 
-    
-    $(".controller").addClass("moveUp"); // Animates range input
+    //Animates Range Input To The Top
+    $(".controller").addClass("moveUp");
     $("input[type=range]").addClass("shrink");
 
     
     markers = [];
 
-    getData(e.target.value); //gets value from (e) and send it to getData as the num parameter
+    getData(e.target.value); 
 
+    // Opens Left List If There Are Searches
     if(document.getElementById("range").innerHTML != 0){
         setTimeout(function(){
-            $('#navigation').prop('checked', true); //slide menu in automatically
+            $('#navigation').prop('checked', true); 
         }, 700);
     }
 
+    // Closes Left List If There Are 0 Searches
     if(document.getElementById("range").innerHTML == 0){
        
             $('#navigation').prop('checked', false)
         
-    };//close side menu if slider value is 0
+    };
 });
 
-// Global variables
-var options = {
-    atmosphere: true,
-    center: [0, 0],
-    zoom: 0
-};
-var earth = new WE.map('earth_div', options);
-var markers = [];
-let stop = false;
 
-function initialize2() { // webGL Adds Satellite Tile + Animation 
-    WE.tileLayer('https://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', { //fetching texture
-        minZoom: 0,
-        maxZoom: 5,
-        attribution: 'NASA'
-    }).addTo(earth);
 
-    var before = null;
-    requestAnimationFrame(function animate(now) {
-        var c = earth.getPosition();
-        var elapsed = before ? now - before : 0; 
-        before = now;
-        earth.setCenter([c[0], c[1] + 0.4 * (elapsed / 30)]); // Setting center to be current lat and long 0.5 is the velocity
-        if (stop) {
-            return
-        };
-        requestAnimationFrame(animate); // recursion for "endless" rotation
-    });
-}
-
+//-------------User Selects List Item
 $("#launchList").on("click", "li", function(e) {
     for (var i = 0; i < markers.length; i++) { //loops through array of markers and removes them from earth element;
 
@@ -165,6 +202,7 @@ $("#launchList").on("click", "li", function(e) {
 
     }
 
+    //MEDIA QUERY
     if (mq.matches) { // closes nav when clicked (MOBILE ONLY)
         $('input#navigation').attr('checked', false);
     }
@@ -181,30 +219,28 @@ $("#launchList").on("click", "li", function(e) {
 
     }
     
-    markers = []; // set markers array to empty
+    markers = [];
 
+    //Isolates selected marker
+    let singleLaunch = allData.launches[$(this).index()]; 
 
-    let singleLaunch = allData.launches[$(this).index()]; // isolates single selected launch from li element. $(this).index() transforms li into array and fetches its array position to be used as the data.launches[] position 
-
+    //Displays ONLY selected marker
     displayMarkers({
         'launches': [singleLaunch]
-    }, false); //runs display marker function for the single selected li element. The "false" parameter stops displayMarkers to run the ("#launchList").append code.
+    }, false); 
 
-    //console.log(singleLaunch);
-
-    //earth.setCenter([[singleLaunch.location.pads[0].latitude], [singleLaunch.location.pads[0].longitude]]);
-
-    var before = null; // not sure if this is necessary, but I'll keep it here.
-    let lat = singleLaunch.location.pads[0].latitude; //Fetches the latitude of the selected li element
-    let long = singleLaunch.location.pads[0].longitude; //Fetches the longitude of the selected li element
+    var before = null;
+    let lat = singleLaunch.location.pads[0].latitude;
+    let long = singleLaunch.location.pads[0].longitude;
 
 
-
-    function calcChange(future, past, times) { //New "smooth" 3d earth focus animation
-        return (future - past) / times; // The difference of the future position (lat or long) divided by "times" which will then be incrementally compensated in the animation recursive loop, this function is called twice (154 and 155) to define variables for lat and long separately.
+    // Calculates The Distance Between Current Center and Marker To Animate The Center of Earth To That Marker When It Is Selected
+    function calcChange(future, past, times) { 
+        return (future - past) / times; 
     }
 
-    let times = 50; // Regulates velocity (50 frames for the animation loop) 
+    //The greate "times" is, the slower the animation will be
+    let times = 50;
     var c = earth.getPosition();
 
     let val1 = calcChange(lat, c[0], times); // Diff of future and past latitude
@@ -213,21 +249,22 @@ $("#launchList").on("click", "li", function(e) {
     var before = null;
     var n = 0;
 
-    requestAnimationFrame(function animate(now) { // animates the "focus" of the list into the earth. ??WHAT IS THE "NOW" PARAMETER DOING??
+    // Animates in a "times" increment
+    requestAnimationFrame(function animate(now) { 
         var c = earth.getPosition();
-        earth.setCenter([c[0] + val1, c[1] + val2]); // Uses the difference between future and past positions, to add to current position and increment it by 50 on n++ recursive loop
+        earth.setCenter([c[0] + val1, c[1] + val2]); 
         n++;
         if (n > times) {
             return
-        }; // Recursive loop stop condition 
-        requestAnimationFrame(animate); // calls itself only until the "times" incrementation is met, giving us the full value of (future-past)/times, so it doesn't snap in position.
-
-
+        }; 
+        requestAnimationFrame(animate);
     });
 
-    stop = true; //Allows user to move earth with its mouse by unlocking position
+    //Allows user to move earth with its mouse by unlocking position
+    stop = true; 
 
-    let streamURL; //Check to see if the launch will have a live stream, if true populate variable with a link, if false populate variable with a <p> instead.
+    //Checks for content, if there is no content add a place holder on Right side infoWindow
+    let streamURL; 
     if (singleLaunch.vidURLs.length == 0) {
         streamURL = "No Stream/ Stream TBA"
     } else {
@@ -248,7 +285,8 @@ $("#launchList").on("click", "li", function(e) {
         agency = "<p><b>Agency: </b><a href='" + singleLaunch.rocket.agencies[0].infoURL + "' target='_blank'>" + singleLaunch.rocket.agencies[0].name + "</a>";
     };
 
-    $("#note").html( //Info Window
+    //Right Side infoWindow Population
+    $("#note").html( 
         "<div class='infoContent'> <h2>Rocket: " + singleLaunch.rocket.name + "</h2>" +
         "<p><b>Launch Date and Time: </b>" + singleLaunch.net  + "</p>" +
         "<p><b>Launch Location: </b>" + singleLaunch.location.name + "</p>" +
@@ -261,109 +299,28 @@ $("#launchList").on("click", "li", function(e) {
 
     // INFO WINDOW SLIDING ANIMATION WITH CSS KEYFRAMES CLASSES
     $("#note div").addClass("infoWindow");
+
     //This will populate the popup once the list item is selected.
-    $("#note").addClass("close");//Close has to come first here so it doesnt impedes open class, it is toggled later to allow the code to run again.
+    $("#note").addClass("close");
     $("#note").addClass("open");
 
-    $("#note").toggleClass("close");// !NOT REDUNDANT close is removed from the default class so it can be toggled later. It seems redundant but it allows the code to run a second time.
+    $("#note").toggleClass("close");
     $("#note").removeClass("displayNone");
 
-    $("#closeWindow").on("click", function() { //X closes window and earth goes back to spinning
-        console.log("clicked");
-        // $("#note").html("");
-        $("#note").toggleClass("close");//this time close is added to the class.
-        $("#note").toggleClass("open");//it has to be removed (toggled) otherwise it wont run a second time
+    //X closes right side window
+    $("#closeWindow").on("click", function() { 
+        $("#note").toggleClass("close");
+        $("#note").toggleClass("open");
         $("#launchList>li").removeClass("active");
         $("#launchList>li>a>p").removeClass("activeCol");
 
 
         stop = false; //Allow animation after closing "focus"/infoWindow
-                    
-        requestAnimationFrame(function animate(now) { //return to animation
-            var c = earth.getPosition();
-            var elapsed = before ? now - before : 0;
-            before = now;
-            earth.setCenter([c[0], c[1] + 0.4 * (elapsed / 30)]);
-            if (stop) {
-                return
-             };
-            requestAnimationFrame(animate);
-        });
         
+        //Enable Animation Again       
+        animation(0.4);
 
-
-
-
-
-        displayMarkers(allData, false); //display markers again
-
+        //Display Markers Again
+        displayMarkers(allData, false);
     });
 });
-
-
-// Clears or Adds "active" class on li element.
-$("#launchList").on("click", "li", function(e){
-  if($("#launchList>li").hasClass("active") && $("#launchList>li>a>p").hasClass("activeCol")){
-    $("#launchList>li").removeClass("active");
-    $("#launchList>li>a>p").removeClass("activeCol");
-    $(this).addClass("active");
-    $(this).children("a").children("p").addClass("activeCol");
-  }else{
-    $(this).addClass("active");
-    $(this).children("a").children("p").addClass("activeCol");
-  }
-});
-
-// When you click the earth it stops animating for a setTimout ammount of times BUG: opening and closing note during the timout causes acceleration;
-$("#earth_div").click(function() {
-     var before = null;
-     stop = true;
-     setTimeout(function(){
-            stop = false; //Allow animation after closing "focus"/infoWindow
-                    
-            requestAnimationFrame(function animate(now) { //return to animation
-                var c = earth.getPosition();
-                var elapsed = before ? now - before : 0;
-                before = now;
-                earth.setCenter([c[0], c[1] + 0.2 * (elapsed / 30)]);
-                if (stop) {
-                    return
-                };
-            requestAnimationFrame(animate);
-        });
-        
-
-        }, 3500);
-
-})
-
-
-
-// Close button, top popup
-// close = document.getElementById("close");
-// close.addEventListener('click', function() {
-//    note = document.getElementById("note");
-//    note.style.display = 'none';
-//  }, false);
-
-
-
-/*TODO:
-
-3- Add google search image API
-4- Add wiki API
-5- Make everything responsive
-
-7- Mobile functionality for tailored UX
-
-
-
-Bonus:
-1- Countdown per launch on popUp window;
-*/
-
-
-
-// Marker Style markers[0].element.style.backgroundColor
-
-
